3c11909cf474e939da2fd7c8cb3ea916c9dd000c
Christian Lugo Ramírez <chrislugram@gmail.com>
2025-06-30 14:41:18 +0200

    feat: define model

--- fireballs/admin.py
+++ fireballs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import FireballEvent
 
-# Register your models here.
+
+@admin.register(FireballEvent)
+class FireballEventAdmin(admin.ModelAdmin):
+    list_display = ("date", "energy", "impact_e", "lat", "lon", "alt", "vel")
+    search_fields = (
+        "date",
+        "lat",
+        "lon",
+    )
--- fireballs/models.py
+++ fireballs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class FireballEvent(models.Model):
+    """
+    This class represents a fireball event
+    """
+
+    date = models.DateTimeField()
+    energy = models.FloatField(null=True, blank=True)
+    impact_e = models.FloatField(null=True, blank=True)
+    lat = models.FloatField(null=True, blank=True)
+    lon = models.FloatField(null=True, blank=True)
+    alt = models.FloatField(null=True, blank=True)
+    vel = models.FloatField(null=True, blank=True)
+
+    class Meta:
+        """
+        Meta class for FireballEvent
+        """
+
+        unique_together = ("date", "lat", "lon")
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f"Fireball {self.date} at ({self.lat}, {self.lon})"
